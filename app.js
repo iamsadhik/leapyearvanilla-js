bd943ec0963fcfc9b88cdf09b7527f8335c98ba5
Ijas mohammed <ijas.mi2714@gmail.com>
2021-01-26 13:07:44 +0530

    feat:update

---
+++ app.js
@@ -0,0 +1,28 @@
+var button=document.querySelector("#btn");
+var inpt=document.querySelector("#input");
+var outPut=document.querySelector("#output");
+
+button.addEventListener("click",clickHandler)
+
+function clickHandler(){
+    var userInput=inpt.value;
+    if(userInput==="")
+    {
+        outPut.innerText= "Please enter any year to check 🙂"
+    }
+    else
+    if(isNaN(userInput))
+    {
+        outPut.innerText= "Enter in specified format 🙂"
+    }
+    else
+    if(userInput %4===0)
+    {
+        outPut.innerText= userInput 
+        + " is a leap year"
+    }
+    else
+    {
+       outPut.innerText= " is not a leap year"
+    }
+}
